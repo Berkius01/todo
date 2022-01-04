9030c42e88f318ee9a31d991dc3a6ea90f7a3645
Berkius01 <58213980+Berkius01@users.noreply.github.com>
2022-01-04 12:56:02 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class='content'>
+    <h1>intro to vue 3</h1>
+    <form @submit.prevent="addTodo">
+      <div class="field">
+      <label class="label">TODO</label>
+      <div class="control">
+          <input v-model="todo" class="input" type="text" placeholder="yabılacak işi gir"/>
+      </div>
+      
+      </div>
+      <button type="submit" class="button is-warning">ekle</button>
+    </form>
+    
+  <div v-for="todo in todos" :key="todo.id" class="card my-5 mx-5">
+    <div class="card-content">
+      <div class="media">
+        <div class="media-left"></div>
+        <div class="media-content">
+          <p :class="{done:todo.done}" @click="done(todo)" class="title is-4 cursor">{{todo.content}}</p>
+          <p class="subtitle is-6">Done: {{todo.done}}</p>
+        </div>
+      </div>  
+    </div>
+  </div>
+</div>
+</template>
+
+
+<script>
+import {ref} from 'vue'
+export default {
+  setup(){
+    const todo = ref('')
+    const todos = ref([])
+
+    function addTodo(){
+      todos.value.push({
+        done: false,
+        content: todo.value,
+        id: Date.now(),
+      })
+      todo.value=''
+    }
+
+    function done(todo){
+      todo.done=!todo.done
+    }
+
+    return{
+      todo,
+      todos,
+      addTodo,
+      done
+   }
+  },
+}
+</script>
+<style Lang="scss">
+  .cursor{
+    cursor: pointer;
+  }
+  .done{
+    text-decoration: line-through;
+  }
+</style>
